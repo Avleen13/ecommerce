e6fd89bd8f496a3b745f104dc02d1399b14baff6
Avleen <n01381031@nj-00118-013.humber.org>
2019-11-19 14:21:07 -0500

    calculations

--- js/index.js
+++ js/index.js
@@ -5,10 +5,14 @@ function minusDetails(){
     let label = document.getElementById(`pantsQty`);
     label.innerHTML = parseInt(label.innerHTML)-1;
 
+
 }
 function plusDetails(){
     let label = document.getElementById(`pantsQty`);
     label.innerHTML = parseInt(label.innerHTML)+1;
+    console.log(label.innerHTML);
+    let extPrice=label.innerHtml*40.99;
+    document.getElementById(`price`).innerHTML += `$ ${extPrice}<br>`;
 }
 minusButton.addEventListener("click",minusDetails);
 plusButton.addEventListener("click",plusDetails);
