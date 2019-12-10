e9be34d82f4ab556b31b8d7ee6417ce68277dc78
Avleen13 <avleen39@gmail.com>
2019-12-10 00:00:29 -0500

    changed to grid

--- js/index.js
+++ js/index.js
@@ -1,149 +1,135 @@
+//Variables
+let decrementButtonPant = document.getElementById("decPro");
+let incrementButtonPant = document.getElementById("plusP");
+let quanPant = document.getElementById("quanPant");
+let decButtonShirt = document.getElementById("decSh");
+let incButtonShirt = document.getElementById("plusS");
+let quanshirt = document.getElementById("quanShirt");
+let pantPriceLabel = document.getElementById("extpricePant");
+let shirtPriceLabel = document.getElementById("extpriceShirt");
 
-let quantity;
-let incBtn = document.getElementById("incrmnt");
-let dcrBtn = document.getElementById("dcrmnt");
-let incBtnFrShrts = document.getElementById("incrmntFrShrts");
-let dcrBtnFrShrts = document.getElementById("dcrmntFrShrts");
-let calcBtn = document.getElementById("Calculate");
-let promoButton = document.getElementById("promoBtn");
-let unitPrice,quantityOfShrts,untPrcFrShrts,extndedPrice,extndPrcFrShrts,totlExtndPrc,tax,totalAmount;
-
-
-function increment() {
-    quantity = parseInt(document.getElementById("quantity").innerHTML);
-    quantity += 1;
-    document.getElementById("quantity").innerHTML = quantity;
-    
-    unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
-    extndedPrice = unitPrice * quantity;
-    document.getElementById("extndpr").innerHTML = extndedPrice;
-}
-
-function decrement() {
-
-    quantity = parseInt(document.getElementById("quantity").innerHTML);
-    quantity--;
-
-    if (quantity < 0) {
-        alert("VALUE SHOULD BE POSITIVE ONLY");
-    }
-
-    else {
-        document.getElementById("quantity").innerHTML = quantity;
-
-        unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
-        extndedPrice = unitPrice * quantity;
-        document.getElementById("extndpr").innerHTML = extndedPrice;
-    }
-}
-
-function incrementForShrts() {
-
-    quantity = parseInt(document.getElementById("quantityOfShrts").innerHTML);
-    quantity += 1;
-    document.getElementById("quantityOfShrts").innerHTML = quantity;
-    
-    unitPrice = parseFloat(document.getElementById("unitPriceForShrts").innerHTML);
-    extndedPrice = unitPrice * quantity;
-    document.getElementById("extndprForShrts").innerHTML = extndedPrice;
-}
-
-function decrementForShrts() {
-
-    quantity = parseInt(document.getElementById("quantityOfShrts").innerHTML);
-    quantity--;
-
-    if (quantity < 0) {
-        alert("VALUE SHOULD BE POSITIVE ONLY");
-    }
-
-    else {
-        document.getElementById("quantityOfShrts").innerHTML = quantity;
-
-        unitPrice = parseFloat(document.getElementById("unitPriceForShrts").innerHTML)
-        extndedPrice = unitPrice * quantity;
-        document.getElementById("extndprForShrts").innerHTML = extndedPrice
-    }
-}
-
-function extendedPriceDisplay(){
-    quantity = parseInt(document.getElementById("quantity").innerHTML);
-    qnttyFrShrts = parseInt(document.getElementById("quantityOfShrts").innerHTML);
-
-    unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML); 
-    untPrcFrShrts = parseFloat(document.getElementById("unitPriceForShrts").innerHTML);
-
-    extndedPrice = quantity * unitPrice;
-    extndPrcFrShrts = qnttyFrShrts * untPrcFrShrts;
-
-    totlExtndPrc = parseFloat((extndedPrice + extndPrcFrShrts).toFixed(2)); console.log(typeof(totlExtndPrc))
-    document.getElementById("subTotal").innerHTML = totlExtndPrc;
-
-    tax = parseFloat((totlExtndPrc * 13/100).toFixed(2)); console.log(typeof(tax));
-    document.getElementById('tax').innerHTML = tax;
-
-    totalAmount = (totlExtndPrc + tax); console.log(typeof(totalAmount));
-    document.getElementById("total").innerHTML = totalAmount;
-}
-
-function getPromoCodeFromUser(){
-
-    let promo = window.prompt("Have a coupon? Please Enter Here! ");
-
-    if(promo == "NOTAX" || promo == "Notax")
-    {
-    quantity = parseInt(document.getElementById("quantity").innerHTML);
-    qnttyFrShrts = parseInt(document.getElementById("quantityOfShrts").innerHTML);
-
-    unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
-    untPrcFrShrts = parseFloat(document.getElementById("unitPriceForShrts").innerHTML);
-
-    extndedPrice = quantity * unitPrice;
-    extndPrcFrShrts = qnttyFrShrts * untPrcFrShrts;
-
-    totlExtndPrc = parseFloat((extndedPrice + extndPrcFrShrts).toFixed(2)) ; 
-    document.getElementById("subTotal").innerHTML = totlExtndPrc;
-
-    tax = 0; 
-    document.getElementById('tax').innerHTML = tax;
-
-    totalAmount = parseFloat((totlExtndPrc + tax).toFixed(2)); console.log(typeof(totalAmount))
-    document.getElementById("total").innerHTML = totalAmount;
-    }
-
-    if(promo == "FIFTYFIFTY" || promo == "Fiftyfifty")
-    {
-    quantity = parseInt(document.getElementById("quantity").innerHTML);
-    qnttyFrShrts = parseInt(document.getElementById("quantityOfShrts").innerHTML);
-
-    unitPrice = parseFloat(document.getElementById("unitPrice").innerHTML);
-    untPrcFrShrts = parseFloat(document.getElementById("unitPriceForShrts").innerHTML);
-
-    extndedPrice = quantity * unitPrice;
-    extndPrcFrShrts = qnttyFrShrts * untPrcFrShrts;
-
-    totlExtndPrc = parseFloat(((extndedPrice + extndPrcFrShrts)*0.5).toFixed(2)) ; 
-    document.getElementById("subTotal").innerHTML = totlExtndPrc;
-
-    tax = parseFloat((totlExtndPrc * 13/100).toFixed(2));
-    document.getElementById('tax').innerHTML = tax;
-
-    totalAmount = parseFloat((totlExtndPrc + tax).toFixed(2));
-    document.getElementById("total").innerHTML = totalAmount;
-    }
-
+//FUNCTIONS
+function increasequanPant() {
+    let qty = parseInt(quanPant.innerHTML) + 1;
+    quanPant.innerHTML = qty;
+    extendedPriceP();
 
 }
 
 
-incBtn.addEventListener("click", increment);
+function descreasequanPant() {
+    if (!parseInt(quanPant.innerHTML) == 0) {
+        let qty = parseInt(quanPant.innerHTML) - 1;
+        quanPant.innerHTML = qty;
+        extendedPriceP();
+    }
+    if (parseInt(quanPant.innerHTML) == 0) {
+        document.getElementById("total").innerHTML = '';
+        document.getElementById("tax").innerHTML = '';
+        document.getElementById("subTotal").innerHTML = '';
 
-dcrBtn.addEventListener("click", decrement);
+    }
 
-incBtnFrShrts.addEventListener("click", incrementForShrts);
+}
 
-dcrBtnFrShrts.addEventListener("click",decrementForShrts);
+function extendedPriceP() {
+    let price = parseInt(quanPant.innerHTML) * 40.99;
+    priceINT = price.toFixed(2);
+    pantPriceLabel.innerHTML = '$' + priceINT;
+    return price;
+}
 
-calcBtn.addEventListener("click",extendedPriceDisplay);
 
-promoButton.addEventListener("click", getPromoCodeFromUser);
+
+function increasequanShirt() {
+    let qty = parseInt(quanshirt.innerHTML) + 1;
+    quanshirt.innerHTML = qty;
+    extendedPriceS();
+
+}
+
+
+function descreasequanShirt() {
+    if (!parseInt(quanshirt.innerHTML) == 0) {
+        let qty = parseInt(quanshirt.innerHTML) - 1;
+        quanshirt.innerHTML = qty;
+        extendedPriceS();
+        if (parseInt(quanshirt.innerHTML) == 0) {
+            document.getElementById("total").innerHTML = '';
+            document.getElementById("tax").innerHTML = '';
+            document.getElementById("subTotal").innerHTML = '';
+
+        }
+    }
+}
+
+function extendedPriceS() {
+    let price = parseInt(quanshirt.innerHTML) * 10.99;
+    priceINT = price.toFixed(2);
+    shirtPriceLabel.innerHTML = '$' + priceINT;
+    return price;
+}
+
+
+function calculateAll() {
+    if (!parseInt(quanshirt.innerHTML) == 0 || !parseInt(quanPant.innerHTML) == 0) {
+        let subtotal = (extendedPriceP() + extendedPriceS()).toFixed(2);
+        subtotal = parseFloat(subtotal);
+        document.getElementById("subTotal").innerHTML = '$' + subtotal;
+        let tax = (0.13 * subtotal).toFixed(2);
+        tax = parseFloat(tax);
+        document.getElementById("tax").innerHTML = '$' + tax;
+        let total = (subtotal + tax).toFixed(2);
+        document.getElementById("total").innerHTML = '$' + total;
+        document.getElementById("subTotal").style.backgroundColor = "transparent";
+        document.getElementById("tax").style.backgroundColor = "transparent";
+    }
+
+}
+
+function applycoupon() {
+    const coupon = window.prompt("please enter the coupon code");
+    const code = ['NOTAX', 'FIFTYFIFTY'];
+    if (coupon == code[0]) {
+        notax();
+    } else if (coupon == code[1]) {
+        fiftyfifty();
+    } else if (coupon == null) {
+        return;
+    } else {
+        alert('invalid coupon');
+    }
+}
+
+function notax() {
+    let subtotal = (extendedPriceP() + extendedPriceS()).toFixed(2);
+    subtotal = parseFloat(subtotal);
+    document.getElementById("subtotal").innerHTML = '$' + subtotal;
+    document.getElementById("tax").innerHTML = '$' + 0;
+    let total = subtotal;
+    document.getElementById("total").innerHTML = '$' + total;
+    document.getElementById("tax").style.backgroundColor = "red";
+    alert("Promo Applied Successfully");
+}
+
+function fiftyfifty() {
+    let subtotal = (extendedPriceP() + extendedPriceS()).toFixed(2);
+    subtotal = parseFloat(subtotal) / 2;
+    document.getElementById("subTotal").innerHTML = '$' + subtotal;
+    document.getElementById("subTotal").style.backgroundColor = "red";
+    let tax = (0.13 * subtotal).toFixed(2);
+    tax = parseFloat(tax);
+    document.getElementById("tax").innerHTML = '$' + tax;
+    let total = subtotal + tax;
+    total = total.toFixed(2);
+    document.getElementById("total").innerHTML = '$' + total;
+    alert("promoCode Applied Successfully");
+}
+
+//event listeners of buttons and execution
+decrementButtonPant.addEventListener("click", descreasequanPant);
+incrementButtonPant.addEventListener("click", increasequanPant);
+decButtonShirt.addEventListener("click", descreasequanShirt);
+incButtonShirt.addEventListener("click", increasequanShirt);
+document.getElementById("calculateBtn").addEventListener("click", calculateAll);
+document.getElementById("promoCode").addEventListener("click", applycoupon);
